8e7c728b651df08b2faeac631f9ea92702c93a3d
slimreap <xt202001871@wmsu.edu.ph>
2022-12-12 03:05:53 +0800

    Update urls.py

--- evotingproject/urls.py
+++ evotingproject/urls.py
@@ -24,8 +24,5 @@ urlpatterns = [
     path('', include('evotingproject.apps.public.urls')),
     path('evoting/', include('evotingproject.apps.evoting.urls')),
     path('accounts/', include('evotingproject.apps.accounts.urls')),
-] 
-
-urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static (settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
 
